29e390a98c4aa0c87c7dc6bed0a4008b4ea0989e
AmirHKarimi888 <amirhkarimi888@gmail.com>
2024-08-27 01:39:53 +0330

    test

--- components/Post.vue
+++ components/Post.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
         {{ selectedPost?.title }}
+
+        <div>
+            {{ x }}
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { pb } from '~/pocketbase';
 import { usePostsStore } from '~/stores/posts';
 
 const postsStore = usePostsStore();
@@ -23,6 +28,11 @@ try {
 } catch (err: any) {
     console.log(err?.message);
 }
+
+const x = ref();
+await pb.collection('posts').getOne(id)
+.then((data) => x.value = data)
+.then(data => console.log(data))
 </script>
 
 <style scoped>
