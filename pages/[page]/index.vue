545e6608fccfb049c7f1357fd86efc2f1445d7fe
AmirHKarimi888 <amirhkarimi888@gmail.com>
2024-09-15 17:27:05 +0330

    Supporting outer contents has been optimized visually

--- pages/[page]/index.vue
+++ pages/[page]/index.vue
@@ -21,7 +21,7 @@
             </Suspense>
         </section>
 
-        <section v-if="selectedPage?.hasCustomContent" id="CustomContent" class="w-full h-screen flex justify-center">
+        <section v-if="selectedPage?.hasCustomContent" id="CustomContent" class="w-full h-screen flex justify-center items-center">
 
         </section>
     </div>
